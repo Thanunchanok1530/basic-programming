e852b625788212da6ad14a9c3b2f26aa01ce385a
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-07-19 16:09:17 +0700

    งาน 2

--- ep1/index.ts
+++ ep1/index.ts
@@ -8,3 +8,12 @@ let ต้องการกลับบ้าน: boolean = true
 console.log(firstName)
 console.log(y)
 console.log(ต้องการกลับบ้าน)
+
+
+
+const a : number = 6
+const b : number = 7
+
+console.log(a==b)
+console.log(a>b)
+console.log(a<=b)
