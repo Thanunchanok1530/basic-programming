617e432499d15ed7cf43df0bbc343d94c4ea0d79
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-07-19 15:33:17 +0700

    ประกาศตัวแปร

---
+++ ep1/index.ts
@@ -0,0 +1,10 @@
+const firstName : string = 'Thanunchanok_Viriyapurmpoonsuk'
+
+let y : number = 10
+y = 20
+let ต้องการกลับบ้าน: boolean = true
+
+
+console.log(firstName)
+console.log(y)
+console.log(ต้องการกลับบ้าน)
