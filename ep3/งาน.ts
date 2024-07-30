3a351da87f8a11b1f6fc5e0738a3ad3f5f393719
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-07-30 19:12:23 +0700

    งาน

--- ep3/งาน.ts
+++ ep3/งาน.ts
@@ -20,9 +20,34 @@ function bmi(high: number, weight: number){
         return 'น้ำหนักอ้วน โรคอ้วนระดับ 2'
     } else if (score > 30) {
         return 'น้ำหนักอ้วนมาก โรคอ้วนระดับ 3'
+    } else {
+        return '-'
     }
 
 }
 
 console.log (bmi(50, 1.65))
+
+
+// ภาวะเสี่ยงต่อโรค
+function disease(high: number, weight: number){
+    let score: number = (weight / (high*high))
+
+    if (score < 18.50) {
+        return 'เสี่ยงโรคมากกว่าคนปกติ'
+    } else if (score >= 18.50-22.90) {
+        return 'เสี่ยงโรคเท่าคนปกติ'
+    } else if (score >= 23-24.90) {
+        return 'อันตรายระดับ 1'
+    } else if (score >= 25-29.90) {
+        return 'อันตรายระดับ 2'
+    } else if (score > 30) {
+        return 'อันตรายระดับ 3'
+    } else {
+        return '-'
+    }
+
+}
+
+console.log (disease(50, 1.65))
 
