6b2f18b42e3ccf60dfe2c6a240ade0caf0abf8ea
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-08-31 15:49:41 +0700

    tkrl

---
+++ ep7/การบ้าน.ts
@@ -0,0 +1,19 @@
+//ข้อมูล(การบ้าน)
+
+const me ={
+    firstName: 'Kaito',
+    lastName: 'Kuroba',
+    age: 17,
+    address: 'Number:17/44 Road:conan Sub-district:Tokyo District:Kumamoto Province:Hokkaido Country:Japan 11550',
+    mobile: '011-444-6688',
+    email: 'Kaitokuro400@gmail.com',
+}
+
+//ประวัติส่วนตัว
+
+//ชื่อ: ไคโตะ
+//นามสกุล: คุโรบะ
+//อายุ: 17
+//ที่อยู่: เลขที่17/44 ถนนโคนัน แขวงโตเกียว เขตคุมาโมโตะ จังหวัดฮอกไกโด ประเทศญี่ปุ่น 11550
+//เบอร์: 011-444-6688
+//อีเมล: Kaitokuro400@gmail.com
