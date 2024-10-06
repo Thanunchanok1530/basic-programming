adcf4eda18518724650c9b20d0e8e8f368111634
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-10-06 22:52:25 +0700

    gy

---
+++ ทวนสอบ/index.ts
@@ -0,0 +1,32 @@
+//Loop
+
+//for Loop (นับจำนวนรอบ)
+//let = ตัวแปร
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+//While Loop
+
+let i = 0;
+while (i <= 10) {
+    console.log('while', i)
+    i++
+}
+
+
+//Array
+//การเอา String , number , boolean มาอยู่ด้วยกัน
+
+const cars = ['Toyota', 'BMW', 'Honda'] //3ตัว
+
+const arr = ['Toyota',2 , true]
+
+cars.length //ความยาวหรือจำนวนสมาชิกกี่ตัว (3ตัว) [จำนวนสมาชิกลบ1=indexที่...]
+
+//หากต้องการเปลี่ยนชื่อสมาชิก
+cars[2] = 'Suzuki'
+
+
+for (let)
