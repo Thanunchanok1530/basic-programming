7977e2d5a6c449dd5691bc586e2780c2e9d738ac
Thanunchanok1530 <thanunchanok0310@gmail.com>
2024-10-08 09:04:26 +0700

    าดก

--- ทวนสอบ/index.ts
+++ ทวนสอบ/index.ts
@@ -198,9 +198,9 @@ function updatePrice
     .filter(function (elemnt, index) {
         return elemnt.price > 150;
     })
-    .map(function(elemnt, index) {
+    .map(function(Element, index) {
         return {
-            price: elemnt.price *1.1,
+            price: Element.price *1.1,
     
         };
     });
@@ -210,4 +210,245 @@ console.log(updatePriceWithForLoop(products))
 console.log(updatePrice(products))
 
 
+
+
+
+//โจทย์ง่าย (เปลี่ยนจาก number เป็น String)
+const number = [1, 2, 3, 4, 5, 6, 7, 8] //['1', '2', '3', '4', '5']
+
+
+//ยาก (กระบวนการวนลูปเหมือนกัน)
+function covertToArrayString(array: number[]) {
+    const result: string[] = [];
+
+    for (let i = 0; i < Array.length; i++) {
+        result.push(Array[i].toString())
+    }
+
+    return result
+}
+
+//ง่ายๆ
+function covertToArrayStringWithMap(array: number[] {
+    return array.map(function(Element, index) {
+        return Element.toString
+    })
+}
+
+console.log(covertToArrayString(number))
+console.log(covertToArrayStringWithMap(number))
+
+
+
+
+
+//โจทย์ปานกลาง (จะแยกชื่อกับนามสกุล Ex.{ firstName: 'John', lastName: 'Doe' },)
+
+const names = ["John Doe", "Jane Smith", "Jack Brown"];
+
+function cutNameWithMap(array: string[]) {
+    return array.map(function(Element, index) {
+        const cutName = Element.split(" ") //['John', 'Doe'] ('indexที่0', 'indexที่1')
+        return {
+            firstName: cutName[0],
+            lastName: cutName[1]
+        }
+    })
+}
+
+console.log(cutNameWithMap(names))
+
+
+//โจทย์ Ex. {"John Doe"}
+const persons = {
+    firstName: 'John', 
+    lastName: 'Doe',
+    age: 21,
+    getFullName: function() {
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+
+//โจทย์ 
+const inventory = {
+    namees: 'Laptop Store',
+    quantity: 0,
+    isAvailable: function() {
+        return this.quantity > 0
+    }
+}
+
+console.log(inventory.isAvailable())
+
+
+
+
+//โจทย์บัญชี (อาจออกข้อสอบ)
+const account = {
+    namess: 'ออมกับสิน',
+    balance: 0,
+    deposit: function(amount: number) {
+        if (amount < 0) {
+            console.log('กรุณาระบุจำนวนเงิน')
+            return
+        }
+        this.balance = this.balance + amount
+        console.log('ฝากเงินจำนวน' + amount + 'ยอดคงเหลือ' + this.balance)
+    },
+    withdraw: function(amount: number) {
+    if (amount > this.balace) {
+        console.log('เงินไม่พอ คุณมียอดเงินคงเหลือ' + this.balace)
+        return
+    }
+
+    this.balace = this.balace - amount
+    console.log('ถอนเงิน' + amount + 'ยอดคงเหลือ' + this.balace)
+    }
+}
+
+account.deposit(699) //ฝาก699 (ฝากเงินจำนวน 699 ยอดคงเหลือ 699)
+account.withdraw(500)  //ถอน500 (ถอนเงิน 500 ยอดคงเหลือ 199)
+
+
+
+
+
+
+
+iloveyou -> il^ve#^* -> iloveyou
+
+i#^love*^#you -> i#^l^ve*##^* -> iyoloveuyyou
+
+
+
+const database = [
+    {
+            email: 'meow@utk.ac.th',
+            password: 'meow@utk0033'
+    }
+]
+
+function login(email: string, password: string){
+    const user = database.filter(function (element, index) {
+        return element.email === email
+    }) 
+
+
+if (user.length > 0) {
+    if (user[0].password === password) {
+        console.log('เข้าสู่ระบบแล้ว')
+    } else {
+        console.log('รหัสผ่านไม่ถูกต้อง')
+    } 
+    } else {
+    console.log('ไม่มีผู้ใช้งานในระบบ')
+    }
+} 
+
+login('meow@utk.ac.th', 'meow@utk0033')
+
+
+//password แบบ Base64
+const database = [
+    {
+            email: 'meow@utk.ac.th',
+            password: 'bWVvd0B1dGswMDMz'
+    }
+]
+
+function login(email: string, password: string){
+    const user = database.filter(function (element, index) {
+        return element.email === email
+    }) 
+
+
+if (user.length > 0) {
+    if (user[0].password === password) {
+        console.log('เข้าสู่ระบบแล้ว')
+    } else {
+        console.log('รหัสผ่านไม่ถูกต้อง')
+    } 
+    } else {
+    console.log('ไม่มีผู้ใช้งานในระบบ')
+    }
+} 
+
+login('meow@utk.ac.th', 'meow@utk0033')
+
+
+
+
+//function login(email: string, password: string){
+//  const user = database.filter(function (element, index) {
+//      return element.email === email
+//    }) 
+//
+//
+//if (email.includes('@') && email.lastIndexOf('@') !== email.length - 1) {
+//    if (user.length > 0){
+//       if (atob(user[0].password) === password) {
+//        alert('เข้าสู่ระบบแล้ว')
+//        } else {
+//        alert('รหัสผ่านไม่ถูกต้อง')
+//        } 
+//    } else {
+//    alert('ไม่มีผู้ใช้งานในระบบ')
+//    }
+//} else {
+//    alert('Mail ไม่ถูกต้อง')
+//}
+//}
+//login('meow@utk.ac.th', 'meow@utk0033')
+
+
+
+
+function login(email: string, password: string) {
+    const user = database.filter(function (element, index) {
+        return element.email === email
+    })
+
+    if (!(email.includes('@') && email.lastIndexOf('@') !== email.length - 1)) {
+    alert('Mail ไม่ถูกต้อง')
+    return
+}
+
+    if (user.length === 0) {
+        alert('ไม่มีผู้ใช้งานในระบบ')
+        return
+    }
+
+    if (!(atob(user[0].password) === password)) {
+        alert('รหัสผ่านไม่ถูกต้อง')
+        return
+    }
+}
+    alert('เข้าสู่ระบบแล้ว')
+
+
+
+
+//Ex.โจทย์ใช้ Loop , while Loop
+
+availability: function () {
+    const availableProducts = []
+    for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i].availability === 'Y') {
+            availableProducts.push(this.data[i])
+        }
+    }
+    return availableProducts
+},
+salePrice: function() {
+    return this.data.filter(function(element, index) {
+        return element.availability === 'Y'
+    }).map(function(element, index) {
+        return ({
+            product_name: element.product_name,
+            sku: element.sku,
+            salePrice: Number(element.price) - element.discount
+        })
+    })
+}
 
